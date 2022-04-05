8d990c4f246e88a4d0d4a10f052f42c6f45a17c4
Hathairat <hathairat.june@nilecon.com>
2022-04-05 16:18:47 +0700

    deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home/Home'
+import About from '../pages/About/About'
 
 export default () => ( 
     <Routes>
       <Route path="/" element={<Home />} />  
+      <Route path="/about" element={<About />} />  
     </Routes>
 )
