aef1b79dc9f10666cfaacf4c425ea8ce4793bac9
Hathairat <hathairat.june@nilecon.com>
2022-04-06 21:02:11 +0700

    deploy

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,11 +4,11 @@ import { Helmet } from 'react-helmet'
 function About() {
   return (
     <div>
-        {/* <Helmet>
+        <Helmet>
             <meta property="og:title" content="บ้านเสนาโซลาร์" data-rh="true"/>
             <meta property="og:description" content="ปลี่ยนพลังงานแสงอาทิตย์ ช่วยให้คุณประหยัดค่าไฟและยังรักษ์โลก โดยช่วยลดก๊าซ CO2 อีกด้วย" data-rh="true"/>
             <meta property="og:image" content="./Artboard.jpg" data-rh="true"/>
-        </Helmet> */}
+        </Helmet>
         <center>About Page</center>
     </div>
   )
