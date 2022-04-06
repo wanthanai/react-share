0040f15940c19a7415c70e47adece3204e5d35a0
Hathairat <hathairat.june@nilecon.com>
2022-04-06 20:58:24 +0700

    deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,13 @@ import { Helmet } from 'react-helmet'
 function Home() {
   return (
     <div>
-        {/* <Helmet>
+        <Helmet>
             <title>Solar</title>
             <meta name="description" content="Helmet application" />
             <meta property="og:title" content="A Step-by-Step Guide to Setting Up Selenium-webdriver with Node.js" data-rh="true"/>
             <meta property="og:description" content="A dummies guide to getting up and running with selenium-webdriver" data-rh="true"/>
             <meta property="og:image" content="https://miro.medium.com/max/812/1*1xhuVp8f2WFUGUByHS8VTg.png" data-rh="true"/>
-        </Helmet> */}
+        </Helmet>
 
         <center>Home Page</center>
     </div>
