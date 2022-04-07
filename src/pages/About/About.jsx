c699b5d01f452cebe2a301859e20ee5535738fe0
Hathairat <hathairat.june@nilecon.com>
2022-04-07 08:50:26 +0700

    deploy

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,11 +4,13 @@ import { Helmet } from 'react-helmet'
 function About() {
   return (
     <div>
-        {/* <Helmet>
-            <meta property="og:title" content="บ้านเสนาโซลาร์" data-rh="true"/>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Solar</title>
+            {/* <meta property="og:title" content="บ้านเสนาโซลาร์" data-rh="true"/>
             <meta property="og:description" content="ปลี่ยนพลังงานแสงอาทิตย์ ช่วยให้คุณประหยัดค่าไฟและยังรักษ์โลก โดยช่วยลดก๊าซ CO2 อีกด้วย" data-rh="true"/>
-            <meta property="og:image" content="./Artboard.jpg" data-rh="true"/>
-        </Helmet> */}
+            <meta property="og:image" content="./Artboard.jpg" data-rh="true"/> */}
+        </Helmet>
         <center>About Page</center>
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,14 @@ import { Helmet } from 'react-helmet'
 function Home() {
   return (
     <div>
-        {/* <Helmet>
+        <Helmet>
+            <meta charSet="utf-8" />
             <title>Solar</title>
-            <meta name="description" content="Helmet application" />
+            {/* <meta name="description" content="Helmet application" />
             <meta property="og:title" content="A Step-by-Step Guide to Setting Up Selenium-webdriver with Node.js" data-rh="true"/>
             <meta property="og:description" content="A dummies guide to getting up and running with selenium-webdriver" data-rh="true"/>
-            <meta property="og:image" content="https://miro.medium.com/max/812/1*1xhuVp8f2WFUGUByHS8VTg.png" data-rh="true"/>
-        </Helmet> */}
+            <meta property="og:image" content="https://miro.medium.com/max/812/1*1xhuVp8f2WFUGUByHS8VTg.png" data-rh="true"/> */}
+        </Helmet>
 
         <center>Home Page</center>
     </div>
